854f462b8a91b5cca35bd3238eca0aaf7cc65ec6
facundo espinosa <facundo.espinosa@fing.edu.uy>
2020-06-13 21:43:08 -0300

    delete fixtures and add mocha and factory bot configurations to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,15 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'minitest/unit'
+require 'mocha/minitest'
+
+FactoryBot.find_definitions
 
 module ActiveSupport
   class TestCase
-    parallelize(workers: :number_of_processors)
+    include FactoryBot::Syntax::Methods
 
-    fixtures :all
+    parallelize(workers: :number_of_processors)
   end
 end
