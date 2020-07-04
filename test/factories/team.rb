b1161bceaa2d638223f8b047dfc69291651090f7
Gabriel Chertok <gchertok@gmail.com>
2020-07-04 10:35:18 -0300

    Change svg for png to better support mobile

--- test/factories/team.rb
+++ test/factories/team.rb
@@ -3,6 +3,6 @@
 FactoryBot.define do
   factory :team do
     name { 'Peñarol' }
-    shield_url { 'https://tcap-api.herokuapp.com/shields/peñarol.svg' }
+    shield_url { 'https://tcap-api.herokuapp.com/shields/penarol.svg' }
   end
 end
